77abbeb11bf8396385b0efb4c41f478829fa035b
Kazuaki Tokunaga <ktokunaga1993@gmail.com>
2023-03-28 22:05:42 +0900

    update

--- codes/tune_parameter.py
+++ codes/tune_parameter.py
@@ -2,26 +2,6 @@ import optuna
 from codes import runner
 
 
-def objective(trial):
-
-    max_depth = trial.suggest_int("max_depth", 3, 9)
-    min_child_weight = trial.suggest_float("min_child_weight", 0.1, 10)
-    colsample_bytree = trial.suggest_float("colsample_bytree", 0.4, 0.95)
-    subsample = trial.suggest_float("subsample", 0.6, 0.95)
-
-    adhoc_params = {
-        'max_depth': max_depth,
-        'min_child_weight': min_child_weight,
-        'colsample_bytree': colsample_bytree,
-        'subsample': subsample
-    }
-
-    run.validation(adhoc_params = adhoc_params)
-    run.evaluate_validation()
-
-    return scores[0]
-
-
 
 def main(load_options, validation_options, model_options):
 
@@ -31,6 +11,27 @@ def main(load_options, validation_options, model_options):
     run.load_dataset()
     run.engineer_features()
     run.delete_df_train()
+
+
+    def objective(trial):
+
+        max_depth = trial.suggest_int("max_depth", 3, 9)
+        min_child_weight = trial.suggest_float("min_child_weight", 0.1, 10)
+        colsample_bytree = trial.suggest_float("colsample_bytree", 0.4, 0.95)
+        subsample = trial.suggest_float("subsample", 0.6, 0.95)
+
+        adhoc_params = {
+            'max_depth': max_depth,
+            'min_child_weight': min_child_weight,
+            'colsample_bytree': colsample_bytree,
+            'subsample': subsample
+        }
+
+        run.validation(adhoc_params = adhoc_params)
+        run.evaluate_validation()
+
+        return scores[0]
+    
 
     study = optuna.create_study(direction="maximize")
     study.optimize(objective, n_trials=10)

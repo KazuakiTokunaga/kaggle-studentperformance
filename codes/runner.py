2da10fce841dcb80812c8388038abf314b843ac3
Kazuaki Tokunaga <ktokunaga1993@gmail.com>
2023-03-28 22:55:14 +0900

    update

--- codes/runner.py
+++ codes/runner.py
@@ -183,8 +183,9 @@ class Runner():
                     else:
                         xgb_params['n_estimators'] = n_estimators_list[t-1]
 
-                    for key, value in adhoc_params.items():
-                        xgb_params[key] = value
+                    if adhoc_params:
+                        for key, value in adhoc_params.items():
+                            xgb_params[key] = value
 
                     if xgb_params.get('early_stopping_rounds'):
                         eval_set = [(valid_x[FEATURES], valid_y['correct'])]
